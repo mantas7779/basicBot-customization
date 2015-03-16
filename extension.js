38e24786094f3e2966114ccac01f8da081f2fcad
mantas7779 <mantas7779@gmail.com>
2015-03-16 11:34:21 +0200

    updatinau skripta

--- extension.js
+++ extension.js
@@ -53,14 +53,14 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
-        language: "english",
+        botName: "Padejėjas Bobas",
+        language: "lithuanian",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupVolume: 10, // 0-100
+        startupEmoji: true, // true or false
         maximumAfk: 120,
-        afkRemoval: true,
+        afkRemoval: false,
         maximumDc: 60,
         bouncerPlus: true,
         lockdownEnabled: false,
@@ -71,19 +71,19 @@
         voteSkip: false,
         voteSkipLimit: 10,
         timeGuard: true,
-        maximumSongLength: 10,
+        maximumSongLength: 8.10,
         autodisable: true,
         commandCooldown: 30,
         usercommandsEnabled: true,
-        lockskipPosition: 3,
+        lockskipPosition: 1,
         lockskipReasons: [
-            ["theme", "This song does not fit the room theme. "],
-            ["op", "This song is on the OP list. "],
-            ["history", "This song is in the history. "],
-            ["mix", "You played a mix, which is against the rules. "],
-            ["sound", "The song you played had bad sound quality or no sound. "],
-            ["nsfw", "The song you contained was NSFW (image or sound). "],
-            ["unavailable", "The song you played was not available for some users. "]
+            ["tema", "Ši daina neatitinka kambario temos. "],
+            ["op", "Ši daina yra OP sąraše. "],
+            ["istorija", "Daina yra istorijoje. "],
+            ["mix", "Jūs paleidote mix`a kuris pažeidžia taisykles. "],
+            ["garsas", "Daina kuria paleidote yra prastos kokybės/negroja. "],
+            ["nsfw", "Daina turi nepadoraus turinio. "],
+            ["nepasiekiama", "Daina nepasiekiama kai kuriems vartotojams"]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
@@ -96,12 +96,12 @@
         opLink: null,
         rulesLink: null,
         themeLink: null,
-        fbLink: null,
+        fbLink: "https://www.facebook.com/groups/712477472146827/",
         youtubeLink: null,
-        website: null,
+        website: "https://www.facebook.com/groups/712477472146827/",
         intervalMessages: [],
         messageInterval: 5,
-        songstats: true,
+        songstats: false,
         commandLiteral: "!",
         blacklists: {
             NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
